6a313c7ed44cd1fc4ccefff50e88195db6d7019c
Ron <zrk08@mail.ru>
2020-10-06 20:17:00 +0300

    Updates

---
+++ precache-manifest.418ff7d092092b6f704aca9d1fc45aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8275c9d7eeea728910ffdc4f53469f",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "765b8dc2f303ab75882e",
+    "url": "/monsters/static/css/main.db86cfa3.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765b8dc2f303ab75882e",
+    "url": "/monsters/static/js/main.d4878c5b.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
